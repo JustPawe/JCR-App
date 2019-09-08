28b0eb38ad7962fc77d80a42500310373769cc37
JustPawe <34323213+JustPawe@users.noreply.github.com>
2019-09-08 16:26:10 +0100

    Created Config class. Created connection to the spreadsheet.

---
+++ app/src/main/java/com/markpaveszka/jcrapp/Config.java
@@ -0,0 +1,7 @@
+package com.markpaveszka.jcrapp;
+
+public class Config {
+
+    public static String google_api_key = "AIzaSyAfwdLu-lrrfqBn31OUUH7llWf6k8GzhRw";
+    public static String spreadsheet_id = "18Cp_lSLWHR1XBG9Otdv6eMCPd9o-LteO1zmVW5QGZn4";
+}
